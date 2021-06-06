256128d1f20497d386e18404d39652b96e8f750b
Md-Sujon <mdsujon829562@gmail.com>
2021-06-06 19:35:49 +0600

    s

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Here To Code</Text>
+      <Text>Sujon</Text>
       <StatusBar style="auto" />
     </View>
   );
